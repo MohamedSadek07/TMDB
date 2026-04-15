be1e6cb8b6c362c320b0dcb1c9e5c88fc656c02b
Mohamed Sadek <mohamedahmeds1994@gmail.com>
2026-04-15 13:21:11 +0200

    Initial commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Mohamed Sadek on 15/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
