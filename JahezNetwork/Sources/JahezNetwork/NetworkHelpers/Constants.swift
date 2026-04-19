7fa3e7813ca46436b2648d526f71829987f19847
Mohamed Sadek <mohamedahmeds1994@gmail.com>
2026-04-19 17:42:18 +0200

    feat(Constants): add Constants file in JahezNetwork that contains baseUrls and token.

---
+++ JahezNetwork/Sources/JahezNetwork/NetworkHelpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  JahezNetwork
+//
+//  Created by Mohamed Sadek on 19/04/2026.
+//
+
+public final class Constants {
+    public static let baseURL = "https://api.themoviedb.org/"
+    public static let postureBaseUrl = "https://image.tmdb.org/t/p/original"
+    public static let authorizationToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDQ0YWU4YTg0ZjAzMzE4OTk1OTEyZjFjZGMyZWYyMiIsIm5iZiI6MTU0OTQ2MzYzOC4yNTUsInN1YiI6IjVjNWFmMDU2OTI1MTQxMzM3Y2I4MWNhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.13k433dyaUMTJEXNfFUKXr0vnenbO0-7cz9uQ3q2eec"
+}
